939b792ecb2fa2d34fc74f623a4eb0a61e0d2427
Taofique <shornob8@gmail.com>
2026-09-08 19:59:27 +0600

    feat: Logger middleware to understand middleware flow

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import express from "express";
 import userRoutes from "./routes/userRoutes.js";
+import logger from "./middleware/logger.js";
 
 const app = express();
 
 // to parse the data from request ( middleware)
 app.use(express.json());
+// Logger middleware to log method and originalUrl
+app.use(logger);
 
 //routes
 app.get("/", (req, res) => {
