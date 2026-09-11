b3b50d2d6dd70ef9042b75994a38b2f71b08d500
Taofique <shornob8@gmail.com>
2026-09-12 00:26:13 +0600

    feat: Add controller and seperate routes linking to controller function

---
+++ controllers/userController.js
@@ -0,0 +1,129 @@
+import User from "../models/User.js";
+
+// get all users
+export const getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.find().select("-password");
+
+    if (users.length < 1) {
+      return res.status(404).json({
+        success: false,
+        message: "Users not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Users fetched successfully",
+      users,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Get user by ID
+export const getUserById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id).select("-password");
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "User found successfully",
+      user,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Create User
+export const createUser = async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = await User.create({
+      name,
+      email,
+      password,
+    }).select("-password");
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      user,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+//Update user
+export const updateUserbyID = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { name, email, password } = req.body;
+    if (!id) {
+      return res.status(404).json({
+        success: false,
+        message: "User ID not found",
+      });
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      id,
+      {
+        name,
+        email,
+        password,
+      },
+      {
+        returnDocument: "after",
+        runValidators: true,
+      },
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "User updated successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+//Delete User by ID
+export const deleteUserbyID = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    if (!id) {
+      res.status(404).json({
+        success: false,
+        message: "User ID not found",
+      });
+    }
+
+    const deletedUser = await User.findByIdAndDelete(id);
+
+    if (!deletedUser) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "User deleted successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,131 +1,19 @@
 import express from "express";
-import User from "../models/User.js";
+
+import {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUserbyID,
+  deleteUserbyID,
+} from "../controllers/userController.js";
 
 const router = express.Router();
 
-// Get all users
-router.get("/", async (req, res, next) => {
-  try {
-    const users = await User.find().select("-password");
-
-    if (users.length < 1) {
-      res.status(404).json({
-        success: false,
-        message: "Users not found",
-      });
-    }
-
-    res.status(200).json({
-      success: true,
-      message: "Users fetched successfully",
-      count: users.length,
-      users,
-    });
-  } catch (error) {
-    next(error);
-  }
-});
-
-// Intentional Route error for checking error handling
-router.get("/error", (req, res, next) => {
-  const error = new Error("Test error");
-  next(error);
-});
-
-// Get single user
-router.get("/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
-
-    const user = await User.findById(id).select("-password");
-
-    if (!user) {
-      res.status(404).json({
-        success: false,
-        message: "User not found",
-      });
-    }
-
-    res.status(200).json({
-      success: true,
-      message: "User found",
-      userData: user,
-    });
-  } catch (error) {}
-});
-
-// POST user (Create User)
-router.post("/", async (req, res, next) => {
-  try {
-    const { name, email, password } = req.body;
-
-    const user = await User.create({
-      name,
-      email,
-      password,
-    });
-
-    res.status(201).json({
-      success: true,
-      message: "User created successfully",
-      user,
-    });
-  } catch (error) {
-    next(error);
-  }
-});
-
-//PUT users (Update a user by ID)
-router.put("/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const { name, email, password } = req.body;
-
-    const updatedUser = await User.findByIdAndUpdate(
-      id,
-      {
-        name,
-        email,
-        password,
-      },
-      {
-        new: true,
-        runValidators: true,
-      },
-    ).select("-password");
-
-    if (!updatedUser) {
-      res.status(404).json({
-        success: false,
-        message: "User not found",
-      });
-    }
-
-    res.status(200).json({
-      success: true,
-      message: "User updated successfully",
-      updatedUser,
-    });
-  } catch (error) {
-    next(error);
-  }
-});
-
-// Delete a user by ID
-router.delete("/:id", async (req, res, next) => {
-  const { id } = req.params;
-  if (!id) {
-    res.status(404).json({
-      success: false,
-      message: "User not found",
-    });
-  }
-  const deletedUser = await User.findByIdAndDelete(id);
-  res.status(200).json({
-    success: true,
-    message: "User deleted successfully",
-    deletedUser,
-  });
-});
+router.get("/", getAllUsers);
+router.get("/:id", getUserById);
+router.post("/", createUser);
+router.put("/:id", updateUserbyID);
+router.delete("/:id", deleteUserbyID);
 
 export default router;
