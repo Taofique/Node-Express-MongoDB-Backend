77f764576f68741eb9929bc02e86745942a084dc
Taofique <shornob8@gmail.com>
2026-09-13 14:46:46 +0600

    feat: password hashing and jwt token setup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,5 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
 export const registerUser = async (req, res, next) => {
@@ -20,15 +22,73 @@ export const registerUser = async (req, res, next) => {
       });
     }
 
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     const user = await User.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     res.status(201).json({
       success: true,
       message: "User registered successfully",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        password: user.password,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Auth Login
+export const loginUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Email and password required",
+      });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        userId: user._id,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h",
+      },
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Login Successful",
+      token,
       user: {
         id: user._id,
         name: user.name,
