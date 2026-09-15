74c64fbdd6ae307db9dddaf70d393e6969df15a2
Taofique <shornob8@gmail.com>
2026-09-15 18:34:22 +0600

    feat: authService fix

--- services/authService.js
+++ services/authService.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import User from "../models/User";
+import User from "../models/User.js";
 
 export const registerUser = async ({ name, email, password }) => {
   const normalizedEmail = email.trim().toLowerCase();
